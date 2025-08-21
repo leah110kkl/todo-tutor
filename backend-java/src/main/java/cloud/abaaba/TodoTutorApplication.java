e0166a10a8c70fad69078434a95577679d4c6b33
Pig-Gua <1270701867@qq.com>
2025-08-21 22:30:56 +0800

    init(backend-java): init java proj

---
+++ backend-java/src/main/java/cloud/abaaba/TodoTutorApplication.java
@@ -0,0 +1,19 @@
+package cloud.abaaba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * TodoTutorApplication
+ *
+ * @author Pig-Gua
+ * @date 2025-08-21
+ */
+@SpringBootApplication
+public class TodoTutorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodoTutorApplication.class, args);
+    }
+
+}
